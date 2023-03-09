3f15ebad0d2123019c3d1aa5bfc0a6fa77c0e2ea
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-03-09 00:13:35 -0300

    att

--- index.js
+++ index.js
@@ -77,12 +77,44 @@ app.delete('/game/:id', (req, res) => {
         var id = parseInt(req.params.id)
 
         var index = DB.games.findIndex(g => g.id == id)
-        
+
         if (index == -1) {
             res.sendStatus(404)
         }else{
             DB.games.splice(index,1)
             res.sendStatus(200)
+        }
+    }
+})
+
+app.put('/game/:id', (req, res) => {
+
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400)
+    }else{
+        var id = parseInt(req.params.id)
+
+        var game = DB.games.find(g => g.id == id)
+
+        if (game != undefined) {
+            var {title, year, price} = req.body
+
+            if (title != undefined) {
+                game.title = title
+            }
+
+            if (price != undefined) {
+                game.price = price
+            }
+
+            if (year != undefined) {
+                game.year = year
+            }
+
+            res.sendStatus(200)
+
+        }else{
+            res.sendStatus(404)
         }
     }
 })
